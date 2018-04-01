df7c00f8431befab233f40b437f30fc91d2e9853
Bacra Woo <bacra.woo@gmail.com>
2018-04-01 20:32:02 +0800

    Update: cmd line

--- aria2/sh.sh
+++ aria2/sh.sh
@@ -6,4 +6,10 @@ DOWNLOAD_DIR=$DIR/../downloads/
 mkdir $DIR/tmp/
 mkidr -p $DOWNLOAD_DIR
 
-$DIR/aria2c --conf-path=$DIR/aria2.conf --dir=$DOWNLOAD_DIR --input-file=$DIR/tmp/aria2.session --save-session=$DIR/tmp/aria2.session --log=$DIR/tmp/aria2-log.log > /dev/null &
+$DIR/aria2c\
+    --conf-path=$DIR/aria2.conf\
+    --dir=$DOWNLOAD_DIR\
+    --input-file=$DIR/tmp/aria2.session\
+    --save-session=$DIR/tmp/aria2.session\
+    --log=$DIR/tmp/aria2-log.log\
+    > /dev/null &
