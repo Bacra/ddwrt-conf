e6489de3a82654e1a7e113679b8dee6e8a97a30d
Bacra Woo <bacra.woo@gmail.com>
2018-04-05 03:21:26 +0800

    Add: install tips

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
-self_dir=./
-self_tmp=$self_dir/.tmp
-mkdir -p $self_dir
+echo -e "\n\n"
+echo " Run in PC\Mac"
+echo " Install Start"
+
+self_tmp=./.tmp
+mkdir -p $self_tmp
 cd $self_tmp
 
 curl -L -o ddwrt-conf.tar.gz https://github.com/Bacra/ddwrt-conf/archive/master.tar.gz
 tar -zxvf ddwrt-conf.tar.gz
 
-mv ddwrt-conf-master/usb_mount $self_dir/usb_mount
+
+echo -e "\n\n"
+echo "Input DDWrt Password"
+scp -r ddwrt-conf-master/usb_mount root@192.168.1.1:/tmp/root
+
 rm -rf $self_tmp
+
+echo -e "\n\n"
+echo " Install Finish"
+echo " /tmp/root/use_mount/main.sh"
