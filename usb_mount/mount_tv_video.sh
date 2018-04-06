6a07a5d7c83ff8b8daa93efcd67b6a06da17eec5
Bacra Woo <bacra.woo@gmail.com>
2018-04-06 16:24:22 +0800

    Update: mount only one time

--- usb_mount/mount_tv_video.sh
+++ usb_mount/mount_tv_video.sh
@@ -4,13 +4,14 @@ TARGET_DIR=/mnt/TV_video
 
 if [ ! -d $TARGET_DIR ]; then
     mkdir $TARGET_DIR
-fi
 
-for i in $(seq 1 5); do
-    self_dir=/mnt/sda$i/TV_video
-    if [ -d "$self_dir" ]; then
-        mount $self_dir $TARGET_DIR
-        echo "Mount TV_video: $self_dir"
-        break
-    fi
-done
+    for i in $(seq 1 5); do
+        self_dir=/mnt/sda$i/TV_video
+        if [ -d "$self_dir" ]; then
+            mount -r $self_dir $TARGET_DIR
+            echo "Mount TV_video: $self_dir"
+            break
+        fi
+    done
+
+fi
