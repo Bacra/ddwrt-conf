24704cb3256b573e4d9d28f1bb31cb163d42e850
Bacra Woo <bacra.woo@gmail.com>
2018-04-06 16:07:11 +0800

    Update: mount tips

--- usb_mount/main.sh
+++ usb_mount/main.sh
@@ -4,8 +4,8 @@ DIR=$(cd `dirname $0`; pwd)
 
 sleep 10
 
-echo "start aria2"
+echo -e "\n\n =====      Start aria2      =====\n"
 $DIR/init_aria2.sh
 
-echo "mount TV_video folder"
+echo -e "\n\n ===== Mount TV_video folder =====\n"
 $DIR/mount_tv_video.sh
